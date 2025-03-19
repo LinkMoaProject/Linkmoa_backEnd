d7013e8919f4cee48d1ffb946d53875e8ade7c74
yachimiya <github.grandkid494@passmail.net>
2025-03-19 20:25:08 +0900

    first commit

---
+++ src/main/java/com/linkmoa/LinkmoaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linkmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkmoaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkmoaBackendApplication.class, args);
+    }
+
+}
